31a2ddaf83f5402f8e6427303a1e695c1cc5663d
joaovictorino <jhvictorino@gmail.com>
2022-05-10 12:24:59 -0300

    elastic search size

--- terraform/main.tf
+++ terraform/main.tf
@@ -26,7 +26,7 @@ resource "azurerm_kubernetes_cluster" "aks-aula-infra" {
 
   default_node_pool {
     name       = "default"
-    node_count = 3
+    node_count = 2
     vm_size    = "Standard_D2_v2"
   }
 
