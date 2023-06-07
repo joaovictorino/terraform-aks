9512478368c61281789a9f09cc3151cb6c27e3c9
joaovictorino <jhvictorino@gmail.com>
2023-06-06 23:41:41 -0300

    hcl file

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,27 +1,27 @@
 terraform {
   required_providers {
     azurerm = {
-        source = "hashicorp/azurerm"
-        version = ">= 3.5.0"
+      source  = "hashicorp/azurerm"
+      version = ">= 3.5.0"
     }
   }
 }
 
 provider "azurerm" {
-    features {
-    }
+  features {
+  }
 }
 
 resource "azurerm_resource_group" "rg-aula-infra" {
-    location = "eastus"
-    name = "rg-aula-infra"
+  location = "eastus"
+  name     = "rg-aula-infra"
 }
 
 resource "azurerm_kubernetes_cluster" "aks-aula-infra" {
-  name                = "aks-aula-infra"
-  location            = azurerm_resource_group.rg-aula-infra.location
-  resource_group_name = azurerm_resource_group.rg-aula-infra.name
-  dns_prefix          = "aks-aula-infra"
+  name                             = "aks-aula-infra"
+  location                         = azurerm_resource_group.rg-aula-infra.location
+  resource_group_name              = azurerm_resource_group.rg-aula-infra.name
+  dns_prefix                       = "aks-aula-infra"
   http_application_routing_enabled = true
 
   default_node_pool {
