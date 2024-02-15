b0cc991c8fb116085298b7dbfb158f40e9c0e6a2
joaovictorino <jhvictorino@gmail.com>
2024-02-14 21:14:07 -0300

    initial commit

--- commands.sh
+++ commands.sh
@@ -2,4 +2,5 @@ az aks get-credentials --resource-group rg-aula-infra --name aks-aula-infra
 
 kubectl config get-contexts
 kubectl config use-context [nome contexto]
+kubectl config use-context [nome contexto] --namespace
 kubectl config unset contexts.[nome contexto]
